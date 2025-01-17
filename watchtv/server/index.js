4ff14fb1463e111be6b4c3ce52bf06f9294314eb
Aaditya Aryal <aadityaryal8@gmail.com>
2025-01-17 21:36:03 +0545

    Some minor changes

--- watchtv/server/index.js
+++ watchtv/server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: 'http://localhost:5180', // Frontend URL
   methods: ['GET', 'POST'], // Add allowed methods
   credentials: true,
 }));
